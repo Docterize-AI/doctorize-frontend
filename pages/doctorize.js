abd888e4b51deb0eae9f7631b66e7cc91b043f86
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-19 21:58:56 -0500

    created input with selection of specific file type

--- pages/doctorize.js
+++ pages/doctorize.js
@@ -2,6 +2,18 @@ export default function Doctorize() {
     return(
         <div>
             <h1>Doctorize</h1>
+            <div>
+                <h3>Upload file</h3>
+                <form>
+                    <input type="file" id="file" name="file" />
+                    <select id="fileType" name="fileType">
+                        <option value="mp4">MP4</option>
+                        <option value="jpg">JPG</option>
+                        <option value="png">PNG</option>
+                    </select>
+                    <input type="submit" value="Doctorize file" />
+                </form>
+            </div>
         </div>
     );
 }
