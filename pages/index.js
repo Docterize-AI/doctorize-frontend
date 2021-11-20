b8199c36a1ad39656804e99082ca24f58c02cdc4
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-19 21:46:20 -0500

    created route to doctorize page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useRouter } from 'next/router';
 
 export default function Home() {
+
+  const routeToDoctorize = () => {
+    const router = useRouter();
+    router.push('/doctorize');
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +20,7 @@ export default function Home() {
 
       <div>
         <h1>Welcome to Doctorize.AI</h1>
-        <button className={styles.button}>Doctorize</button>
+        <button className={styles.button} onClick={routeToDoctorize}>Doctorize</button>
       </div>
     </div>
   )
