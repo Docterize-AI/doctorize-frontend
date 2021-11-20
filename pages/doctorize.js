a44d8c088e866c55f34752bb7f6ba05400f5ca21
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-19 22:40:19 -0500

    tested file state

--- pages/doctorize.js
+++ pages/doctorize.js
@@ -6,18 +6,24 @@ export default function Doctorize() {
     const [file, setFile] = useState(null);
     const [fileType, setFileType] = useState(null);
 
-    const doctorize = async () => {
+    const doctorize = async (e) => {
+        e.preventDefault();
         const data = new FormData();
         data.append('file', file);
         const response = await axios.post('http://localhost:5000/upload', data);
         console.log(response.data);
+        console.log(file, fileType);
     }
 
     const onChangeHandler = (e) => {
+        e.preventDefault();
+        console.log(e.target.files[0]);
         setFile(e.target.files[0]);
     }
 
     const fileTypeChange = (e) => {
+        e.preventDefault();
+        console.log(e.target.value);
         setFileType(e.target.value);
     }
 
@@ -27,12 +33,12 @@ export default function Doctorize() {
             <div>
                 <h3>Upload file</h3>
                 <form>
-                    <input type="file" id="file" name="file" />
+                    <input type="file" id="file" name="file" onChange={onChangeHandler}/>
                     <select id="fileType" name="fileType" onChange={fileTypeChange}>
                         <option value='image'>Image</option>
                         <option value='audio'>Audio</option>
                     </select>
-                    <input type="submit" value="Doctorize file" onChange={onChangeHandler}/>
+                    <input type="submit" value="Doctorize file" onChange={doctorize}/>
                 </form>
             </div>
         </div>
