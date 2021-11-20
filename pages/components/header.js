c8930ae0feda6d65550df701152abcc9b65e47b3
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-20 11:59:04 -0500

    styled buttons to the right

--- pages/components/header.js
+++ pages/components/header.js
@@ -2,11 +2,13 @@ import styles from '../../styles/Home.module.css';
 
 export const Header = () => {
     return (
-        <div>
+        <div className={styles['header-container']}>
             <h1>Doctorize</h1>
+            <div className={styles['buttons-container']}>
             <button className={styles.button}>Login</button>
             <button className={styles.button}>Request a Doctor</button>
             <button className={styles.button}>About Us</button>
+            </div>
         </div>
     );
 }
