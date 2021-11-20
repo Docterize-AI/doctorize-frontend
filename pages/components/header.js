a48a21ee2bacac4a3fc72d1080c2b686fb9f5e88
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-20 11:49:53 -0500

    created button styling

--- pages/components/header.js
+++ pages/components/header.js
@@ -1,10 +1,12 @@
+import styles from '../../styles/Home.module.css';
+
 export const Header = () => {
     return (
         <div>
             <h1>Doctorize</h1>
-            <button>Login</button>
-            <button>Request a Doctor</button>
-            <button>About Us</button>
+            <button className={styles.button}>Login</button>
+            <button className={styles.button}>Request a Doctor</button>
+            <button className={styles.button}>About Us</button>
         </div>
     );
 }
