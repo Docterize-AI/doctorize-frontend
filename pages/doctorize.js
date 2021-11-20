4f5e9e96d0f5bdf8b0ac22979684238fb0e63339
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-19 22:33:45 -0500

    created file state

--- pages/doctorize.js
+++ pages/doctorize.js
@@ -1,10 +1,24 @@
 import axios from 'axios';
+import { useState } from 'react';
 
 export default function Doctorize() {
 
+    const [file, setFile] = useState(null);
+    const [fileType, setFileType] = useState(null);
+
     const doctorize = async () => {
-        const response = await axios.get('http://localhost:5000/doctorize');
+        const data = new FormData();
+        data.append('file', file);
+        const response = await axios.post('http://localhost:5000/upload', data);
         console.log(response.data);
+    }
+
+    const onChangeHandler = (e) => {
+        setFile(e.target.files[0]);
+    }
+
+    const fileTypeChange = (e) => {
+        setFileType(e.target.value);
     }
 
     return(
@@ -14,11 +28,11 @@ export default function Doctorize() {
                 <h3>Upload file</h3>
                 <form>
                     <input type="file" id="file" name="file" />
-                    <select id="fileType" name="fileType">
-                        <option>Image</option>
-                        <option>Audio</option>
+                    <select id="fileType" name="fileType" onChange={fileTypeChange}>
+                        <option value='image'>Image</option>
+                        <option value='audio'>Audio</option>
                     </select>
-                    <input type="submit" value="Doctorize file" />
+                    <input type="submit" value="Doctorize file" onChange={onChangeHandler}/>
                 </form>
             </div>
         </div>
