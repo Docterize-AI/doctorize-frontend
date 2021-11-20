5bc36e90cd4fc4e0bf1b8aea49826223a6f4fa8b
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-11-20 16:38:54 -0500

    completed routes to header buttons

--- pages/components/header.js
+++ pages/components/header.js
@@ -4,14 +4,28 @@ import logo from './images/image.jpeg';
 import { useRouter } from 'next/router';
 
 export const Header = () => {
+
+    const router = useRouter();
+    const routeToRequestDoctor = () => {
+        router.push('/request_doctor');
+    }
+
+    const routeToAbout = () => {
+        router.push('/about');
+    }
+
+    const routeToLogin = () => {
+        router.push('/');
+    }
+
     return (
         <div className={styles['header-container']}>
             <Image src={logo} alt="logo goes here" width={100} height={95}/>
             <h1>Doctorize</h1>
             <div className={`${styles['buttons-container']}`}>
-            <button className={`${styles.button} ${styles.yellow}`} onClick={() => }>Request a Doctor</button>
-            <button className={`${styles.button} ${styles.yellow}`}>About Us</button>
-            <button className={`${styles.button} ${styles.red}`}>Login</button>
+            <button className={`${styles.button} ${styles.yellow}`} onClick={routeToRequestDoctor}>Request a Doctor</button>
+            <button className={`${styles.button} ${styles.yellow}`} onClick={routeToAbout}>About Us</button>
+            <button className={`${styles.button} ${styles.red}`} onClick={routeToLogin}>Login</button>
             </div>
         </div>
     );
